981f9cff9cb1364e71ad8db26fd76d856a3100bf
evanoffPNNL <50331146+evanoffPNNL@users.noreply.github.com>
2019-08-27 13:56:20 -0400

    Updated UI

--- inst/shinyApp/ui.r
+++ inst/shinyApp/ui.r
@@ -6,7 +6,7 @@ fluidPage(theme = shinythemes::shinytheme("darkly"),
   shinythemes::themeSelector(),
   shinyalert::useShinyalert(),
   # Application title
-  titlePanel("Hector User Interface"),
+  titlePanel("Hector Interactive Climate Model"),
 
   # Function that gets called on first load of application to load in any themes/css etc
   tags$head
@@ -16,7 +16,7 @@ fluidPage(theme = shinythemes::shinytheme("darkly"),
   # Main component from which all other components fall under, navbarPage.
   navbarPage
   (
-    "Navigation",
+    "Navigation:",
     # multi-page user-interface that includes a navigation bar.
     tabPanel
     (
@@ -25,25 +25,25 @@ fluidPage(theme = shinythemes::shinytheme("darkly"),
       (
         width=3,
         div(h4("Baseline Scenarios"),#, icon("info-circle", "fa-1x")),
-            tags$hr(style="border-color: #375a7f;"),
+            tags$hr(class="hrNav"),
             selectInput("input_RCP", "RCP Scenario", list("2.6" = "RCP 2.6","4.5"="RCP 4.5", "6"="RCP 6.0", "8.5" = "RCP 8.5"), width=200, selected = "RCP 4.5"),
 
             # radioButtons("input_Driven", "", list("Emissions Driven"), inline=TRUE),
             textInput("input_ScenarioName", "Scenario Name", width=200, value = ""),
 
         div(h4("Custom Scenario"),
+            tags$hr(class="hrNav"),
             fileInput("input_ScenarioFile", "Upload Emissions File", width=275, buttonLabel = "Choose File", accept = c("text/csv", ".csv", "text/comma-separated-values,text/plain")),
             actionButton(inputId="input_Driven", label="Load Scenario"))
         ),
-        div
-        (
-          h4("Model Parameters"),
+        div(h4("Model Parameters"),
+          tags$hr(class="hrNav"),
           numericInput("input_aero", "Aerosol forcing scaling factor (unitless)", width=275, value = NA, step = 0.01),
           numericInput("input_beta", "CO2 fertilization factor (unitless)", width=275, value = NA, step = 0.01),
           numericInput("input_diff", "Ocean heat diffusivity (cm2/s)", width=275, value = NA, step = 0.01),
-          numericInput("input_ecs", "Equilibrium Climate Sensitivity (degC)", width=275, value = NA, step = 0.01),
+          numericInput("input_ecs", "Equilibrium climate sensitivity (degC)", width=275, value = NA, step = 0.01),
           numericInput("input_pco2", "Preindustrial CO2 concentration (ppmv CO2)", width=275, value = NA, step = 0.01),
-          numericInput("input_q10", "Heterotrophic respiration temp sensitivity factor (unitless)", width=275, value = NA, step = 0.01),
+          numericInput("input_q10", "Temperature sensitivity factor (Q10) (unitless)", width=275, value = NA, step = 0.01),
           numericInput("input_volc", "Volcanic forcing scaling factor (unitless)", width=275, value = NA, step = 0.01),
           actionButton(inputId="set_Params", label="Set Parameters"),
           actionButton(inputId="reset_Params", label="Reset Parameters")
@@ -57,7 +57,9 @@ fluidPage(theme = shinythemes::shinytheme("darkly"),
           tabPanel
           (
             p(icon("info-circle", "fa-2x"), " System Information", value="infoTab"),
-            p(br(),"Hector Shiny is a web-based user interface designed interact with Hector, an open source, object-oriented, simple global climate carbon-cycle model.
+            h5("System Information"), tags$hr(class="hrNav"),
+            p("The Hector Interactive Climate Model is a web-based user interface designed to interact with Hector, an open source, object-oriented, simple global climate carbon-cycle model
+            built in C++ with an R package based interface. This model was originally designed to run through R scripts, but is now web enabled to reach a broader audience.
 It  runs essentially instantaneously while still representing the most critical global scale earth system processes,
 and is one of a class of models heavily used for for emulating complex climate models and uncertainty analyses."),
             p("
@@ -70,8 +72,8 @@ Please note that the wiki documents are included in the repository as a ", a("Gi
 Submodule",href="https://git-scm.com/book/en/v2/Git-Tools-Submodules", target="blank"),"."),
             p("To clone the repository as well as the documentation,
 you can use `git clone --recursive`. If you have already cloned non-recursively, you can fetch (download) the documentation
-by running `git submodule init; git submodule update`. The code is also well documented with ", a("Doxygen", href="http://doxygen.org", target="blank"), "-style
-comments."),tags$hr(style="border-color: #375a7f;"),
+by running `git submodule init; git submodule update`. The code is also well documented with ", a("Doxygen-style", href="http://doxygen.org", target="blank"), "
+comments."),
             p("A formal model description paper ", a("Hartin et al. 2015", href="http://www.geosci-model-dev.net/8/939/2015/gmd-8-939-2015.html", target="blank"),
 "documents its science internals and performance relative to observed data, the ", a("CMIP5", href="http://cmip-pcmdi.llnl.gov/cmip5/", target="blank"), "archive,
 and the reduced-complexity ", a("MAGICC", href="http://www.magicc.org", target="blank"), " model.
@@ -88,13 +90,17 @@ Battelle Memorial Institute under contract DE-AC05-76RL01830."),
               interface to Hector.")
             )
           ),
-          tabPanel( fluid=TRUE, p(icon("chalkboard-teacher", "fa-2x"), " Instructions", value="infoTab")),
+          tabPanel( fluid=TRUE, p(icon("chalkboard-teacher", "fa-2x"), " Instructions", value="infoTab"),
+                    h5("How the system works"), tags$hr(class="hrNav")
+
+                  ),
           tabPanel
           ( fluid = TRUE,
             p(icon("chart-line","fa-2x"), " Scenario Output", value="outputTab"),
             # Output Tab Panel
-            mainPanel
-            (
+            # mainPanel
+            # (
+            h5("Scenario Results"), tags$hr(class="hrNav"),
               p(selectInput
                 ("capabilities", "Choose capability identifiers:",
                   list('Carbon Cycle' = list("Atmospheric CO2"="cc_co2", "Atmospheric Carbon Pool"="cc_acp"),
@@ -114,13 +120,13 @@ Battelle Memorial Institute under contract DE-AC05-76RL01830."),
                        #                "Partial Pressure CO2 - High Lat"='o_hl_pp_co2',"Partial Pressure CO2 - Low Lat"='o_ll_pp_co2',"Dissolved Inorganic C - High Lat"='o_hl_dic', "Dissolved Inorganic C - Low Lat"='o_ll_dic', "Ocean Temperature - High Lat"='o_hl_t', "Ocean Temperature - Low Lat"='o_ll_t',
                        #                "Carbonate Concentration - High Lat"='o_hl_cc', "Carbonate Concentration - Low Lat"='o_ll_cc'),
                        'SO2' = list( "Anthropogenic SO2"='so2_a', "Natural CH4 Emissions"='so2_n_ch4', "Volcanic SO2"='so2_v'),
-                       'Temperature' = list("Global Mean Temp"='t_gmt', "Equilibrium Global Temp"='t_egt', "Ocean Surface Temp"='t_ost', "Ocean Air Temp"='t_oat', "Land Temp Anomaly"="t_lta", "Heat Flux - Mixed Layer Ocean"='t_hf_mlo', "Heat Flux - Interior Layer Ocean"='t_hf_ilo', "Total Heat Flux - Ocean"='t_hf_t')                  ), multiple = T
+                       'Temperature' = list("Global Mean Temp"='t_gmt', "Equilibrium Global Temp"='t_egt', "Ocean Surface Temp"='t_ost', "Ocean Air Temp"='t_oat', "Land Temp Anomaly"="t_lta", "Heat Flux - Mixed Layer Ocean"='t_hf_mlo', "Heat Flux - Interior Layer Ocean"='t_hf_ilo', "Total Heat Flux - Ocean"='t_hf_t')), multiple = T, width=350
                 ),  actionButton(inputId="loadGraphs", label="Load Graphs", width = 200), downloadButton("dlData", label="Download Raw Data")
                ),
               # plotOutput("plot"),
-              plotly::plotlyOutput("plot2", width = "800")
+              plotly::plotlyOutput("plot2", width = "100%", height = "500")
                #div(width = '100%', plotOutput("plot"))
-            ) # end mainpanel
+            # ) # end mainpanel
           ) # end tabpanel
         ) # end tabsetpanel
       ), # end mainpanel
