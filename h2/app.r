c776483c6c89e4dd1baba7ee19b466a3d9095383
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2023-12-18 13:53:47 -0500

    Add header

--- h2/app.r
+++ h2/app.r
@@ -2,6 +2,15 @@ source("./global.r")
 
 ui <- fluidPage(
   includeCSS("./components/layout/style copy.css"),
+  tags$div(class = "container",
+           tags$img(src = "images/earth-header.png", height = "300px", width = "100%", class = "earth", alt = "Earth's atmosphere"),
+           tags$div(
+               a(
+                   img(src = "images/GCIMS_logo_alt.svg", class = "logo"), href = "https://gcims.pnnl.gov/", target = "_blank"),
+               h1("HectorUI", class = "header-text-title"),
+               h2("An Interactive Climate Model", class = "header-text-sub", style = "font-weight:normal; "),
+           ),
+  ),
   navbarPage(
     id = "nav",
     title = "",
@@ -9,7 +18,7 @@ ui <- fluidPage(
     tabPanel(title = "Home",
              includeHTML("./components/layout/homepage.html")),
     tabPanel(title = "Guides"),
-    tabPanel(title = "Explore Hector",
+    tabPanel(title = "Run Hector",
              fluidRow(
                  run_ui("run_1"),
                  #download_ui("download_1"),
