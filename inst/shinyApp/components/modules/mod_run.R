918657e83e5766b88ab2d18717e88e23a1459d7b
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2024-05-31 19:03:44 -0400

    Add ssp and version to param downl

--- inst/shinyApp/components/modules/mod_run.R
+++ inst/shinyApp/components/modules/mod_run.R
@@ -198,6 +198,8 @@ run_server <- function(id, r6) {
                     if(!is.null(r6$output))
                     {
                         header_text <- paste0("File created with Hector UI - https://github.com/JGCRI/hector-ui accessed on ", format(Sys.time(), "%Y-%m-%d %X"), "\n",
+                                             "Hector Version: ", packageVersion("hector"), "\n",
+                                             "SSPs Used: ", names(which(scenarios == input$ssp_path, arr.ind = FALSE)), "\n",
                                              "Permafrost: ", r6$permafrost, "\n",
                                              "Model Parameters: " , input$input_paramToggle , "\n",
                                              "Alpha: ", input$alpha,  "\n",
