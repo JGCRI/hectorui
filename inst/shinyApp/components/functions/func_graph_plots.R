c4d63f279c85992fbd5268648fe424c031688e43
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2024-06-05 15:22:55 -0400

    Add units to plot

--- inst/shinyApp/components/functions/func_graph_plots.R
+++ inst/shinyApp/components/functions/func_graph_plots.R
@@ -2,10 +2,12 @@
 
 graph_plots <- function(r6) {
 
+    var <- last(r6$output)$variable[1]
+    unit <- last(r6$output)$unit[1]
 
         {ggplot(r6$output) +
                 geom_line(aes(x = year, y = value, color = Scenario)) +
-                labs(x = "Year", y = last(r6$output)$variable[1],
+                labs(x = "Year", y = paste0(title[[var]], " (", unit, ")"),
                      title = paste0("Variable: ", last(r6$output)$variable[1],"\nPermafrost: ",r6$permafrost, "\n")) +
                 theme(legend.position = "bottom")} %>%
             plotly::ggplotly()
